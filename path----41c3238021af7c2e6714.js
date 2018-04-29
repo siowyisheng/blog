f7b116dd7af01684d3c7ffb8274fb7e123bd8967
Yi Sheng Siow <siowyisheng@gmail.com>
2018-04-30 01:08:28 +0800

    Updates

---
+++ path----41c3238021af7c2e6714.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{109:function(t,e){t.exports={data:{site:{siteMetadata:{title:"yisheng's blog"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----41c3238021af7c2e6714.js.map
---
+++ path---my-first-md-blog-post-ae885a44fb7112b103cb.js
@@ -0,0 +1,2 @@
+webpackJsonp([28777423234503],{359:function(e,t){e.exports={data:{markdownRemark:{html:'<p>Pandas are really sweet.</p>\n<p>Here\'s a video of a panda eating sweets.</p>\n<iframe width="560" height="315" src="https://www.youtube.com/embed/4n0xNbfJLR8" frameborder="0" allowfullscreen></iframe>',frontmatter:{title:"Sweet Pandas Eating Sweets"}}},pathContext:{slug:"/my-first-md-blog-post/"}}}});
+//# sourceMappingURL=path---my-first-md-blog-post-ae885a44fb7112b103cb.js.map
