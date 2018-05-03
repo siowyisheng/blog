05ff1dc333950e7a64bd3f4c10d44d8379d5227b
Yi Sheng Siow <siowyisheng@gmail.com>
2018-05-04 00:42:00 +0800

    Updates

--- path---ux-best-practices-0fe4b131bfc65fe23a99.js
+++ path---ux-best-practices-0fe4b131bfc65fe23a99.js
@@ -1,2 +1,2 @@
-webpackJsonp([0xe72407eb0307],{363:function(t,e){t.exports={data:{markdownRemark:{html:"<h4>Regular users</h4>\n<p>Clicks to</p>\n<p>Easy clicks</p>\n<p>Hard clicks</p>\n<h4>Power users</h4>\n<p>Keystrokes to</p>",frontmatter:{title:"UX best practices"}}},pathContext:{slug:"/ux-best-practices/"}}}});
+webpackJsonp([0xe72407eb0307],{378:function(t,e){t.exports={data:{markdownRemark:{html:"<h4>Regular users</h4>\n<p>Clicks to</p>\n<p>Easy clicks</p>\n<p>Hard clicks</p>\n<h4>Power users</h4>\n<p>Keystrokes to</p>",frontmatter:{title:"UX best practices"}}},pathContext:{slug:"/ux-best-practices/"}}}});
 //# sourceMappingURL=path---ux-best-practices-0fe4b131bfc65fe23a99.js.map
